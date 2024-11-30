58a4959d4eb9b915177b64e1a4c9cd18351fe88a
Leo <leonardo.manara04@gmail.com>
2024-11-30 20:52:43 +0100

    FANEQLI ALMOST OVER

--- projetICC.cpp
+++ projetICC.cpp
@@ -54,7 +54,7 @@ void verification_1(string& display_type, int& nbF) {
 
 }  
 
-void verification_2_liste_queues(vector<queue<int> >& liste_queues, int nbF, vector<int> &taille_queue) {
+void verification_2_liste_queues(vector<queue<int> >& liste_queues, vector<queue<int> >& liste_queues_F, int nbF, vector<int> &taille_queue) {
 
     int file_attente_arrive, couloir_sortie = 0 ; 
 
@@ -72,6 +72,7 @@ void verification_2_liste_queues(vector<queue<int> >& liste_queues, int nbF, vec
         }
 
         liste_queues[file_attente_arrive].push(couloir_sortie) ;
+        liste_queues_F[file_attente_arrive].push(couloir_sortie) ;
         taille_queue[file_attente_arrive] = taille_queue[file_attente_arrive] + 1 ;
 
         cin >> file_attente_arrive >> couloir_sortie  ;
@@ -83,17 +84,19 @@ void verification_2_liste_queues(vector<queue<int> >& liste_queues, int nbF, vec
 
 // construction liste de bool , si file d atztente vide alors bool vrai sinon faux 
 
-void construction_liste_bool(vector<queue<int> >liste_queues, int nbF , vector<bool> &liste_bool) {
+void construction_liste_bool(vector<queue<int> >liste_queues, int nbF , vector<bool> &liste_bool_N, vector<bool> &liste_bool_F) {
 
     for (int i = 0 ; i < nbF ; i ++ ) {
 
         if (liste_queues[i].empty()) {
 
-            liste_bool[i] = true ; 
+            liste_bool_N[i] = true ; 
+            liste_bool_N[i] = true ;
 
         } else {
 
-                liste_bool[i] = false  ; 
+                liste_bool_N[i] = false  ; 
+                liste_bool_F[i] = false  ;
             }
 
     }
@@ -106,12 +109,12 @@ bool all_empty(vector<bool> liste_bool, int nbF) {
         
         if (!liste_bool[j]) {
 
-            return true ;
+            return false ;
         }
 
     }
 
-    return false ;
+    return true ;
 }
 
 void affiche_etat_initial (vector<queue<int> > liste_queues, int nbF) {
@@ -154,7 +157,7 @@ int i_non_vide(vector<bool> &liste_bool, int nbF) {
 
 
 
-void verification_premiere_liste(vector<bool> &liste_bool, int nbF, int &i, int &nbC1, int &d1, string display_type) {
+void verification_premiere_liste(vector<bool> liste_bool, int nbF, int &i, int &nbC, int &d, string display_type) {
 
     if (liste_bool[0]) {
 
@@ -168,8 +171,8 @@ void verification_premiere_liste(vector<bool> &liste_bool, int nbF, int &i, int 
 
             cout << i << "      " << "0 0" << endl ;
         } 
-        nbC1 = nbC1 + 1 ;
-        d1 = d1 + i  ;      
+        nbC = nbC + 1 ;
+        d = d + i  ;      
     }
 }
 
@@ -202,7 +205,7 @@ void extraction_neqli(vector<queue<int> > &liste_queues, vector<double> &attente
 
 }
 
-bool extraction_Neqli_2(vector<bool> &liste_bool, int nbF, int &i, int c, int &nbC1, int &d1, bool &iscan, string display_type) {
+bool extraction_Neqli_2(vector<bool> liste_bool, int nbF, int &i, int c, int &nbC1, int &d1, bool &iscan, string display_type) {
 
     if (liste_bool[i]) {
 
@@ -233,7 +236,7 @@ bool extraction_Neqli_2(vector<bool> &liste_bool, int nbF, int &i, int c, int &n
     return false ; 
 }
 
-void NEQLI(vector<queue<int> >liste_queues, vector<bool> &liste_bool, int &nbC1, int nbF, int &d1, vector<double> &attente_moyenne_NEQLI, string display_type) {
+void NEQLI(vector<queue<int> >liste_queues, vector<bool> liste_bool, int &nbC1, int nbF, int &d1, vector<double> &attente_moyenne_NEQLI, string display_type) {
 
     int i = 0 ;
     int c = 0 ;
@@ -249,11 +252,6 @@ void NEQLI(vector<queue<int> >liste_queues, vector<bool> &liste_bool, int &nbC1,
 
     while (i < nbF ) {
            
-        if (i >= nbF ) {
-            
-            break ; 
-        }
-
         extraction_neqli(liste_queues, attente_moyenne_NEQLI, i, c , nbC1, d1, iscan, display_type) ; 
         
         iscan = true ;
@@ -273,7 +271,132 @@ void NEQLI(vector<queue<int> >liste_queues, vector<bool> &liste_bool, int &nbC1,
 }
 
 
-void affiche_attente_moyenne(int nbF, vector<double> attente_moyenne_NEQLI, vector<int> taille_queue) {
+ void construction_done(vector<bool> liste_bool, vector<bool> &done, int nbF) {
+
+    for (int j = 0 ; j < nbF ; j++) {
+
+        if (!liste_bool[j]) {
+
+            done[j] = false ;
+
+        }  else {
+
+            done[j] = true ; 
+        }
+    }
+}
+
+
+void FANEQLI(vector<queue<int> >&liste_queues_F, vector<bool> &liste_bool_F, vector<bool> done, int &nbC2, int nbF, int &d2, vector<double> &attente_moyenne_FANEQLI, string display_type) {
+
+    int i = 0, j = 0 , c = 0 ;
+    bool iscan = false  ; 
+    nbC2 = 1 ;
+
+    if (display_type == "SHOW_CYCLES"){
+
+        cout << "FANEQLI" << endl ;
+    }
+
+    verification_premiere_liste(liste_bool_F, nbF, j, nbC2, d2, display_type) ;
+
+
+    while (i < nbF) {
+
+        construction_done(liste_bool_F , done, nbF) ;
+
+        while (j < nbF) {
+
+            if (done[j]) {
+                
+                i = i_non_vide(done, nbF) ; 
+
+                if (i == nbF) {
+                    break ; 
+                } else {
+
+                    cout << j <<"       " ; 
+                    d2 = d2 + abs(i-j) ; 
+                    nbC2 = nbC2 + 1 ; 
+                    j = i;
+                    cout << j <<"      " << "0 1" << endl ;
+                    
+                   
+                }
+            }
+
+            if (j == nbF) {
+                break ; 
+            }
+
+            c = liste_queues_F[j].front() ;
+            liste_queues_F[j].pop() ;
+
+            attente_moyenne_FANEQLI[j] = attente_moyenne_FANEQLI[j] + nbC2 ;
+            d2 = d2 + abs(j-c) ; 
+            nbC2 = nbC2 + 1 ;
+            
+            cout << j <<"       "<< c << "      " ; 
+
+            if (!iscan) {
+
+                cout << "0 1" << endl ;
+
+            }  else { 
+
+                cout << "1 1" << endl ;
+
+            } 
+
+            iscan = true ; 
+
+            done[j] = true ; 
+            if (liste_queues_F[j].empty()) {
+
+                liste_bool_F[j] = true ; 
+
+            }
+
+            j = c ; 
+
+            if (all_empty(done, nbF)) {
+
+                break ; 
+            }
+
+            if (done[j]) {
+                cout << j << "       " ;
+
+                d2 = d2 + abs(j-i_non_vide(done, nbF)) ; 
+                nbC2 = nbC2 + 1 ;
+
+                j = i_non_vide(done, nbF) ; 
+                iscan = false ; 
+                cout << j << "      " << "1 0" << endl ; 
+            }
+
+
+            if (all_empty(liste_bool_F, nbF)) {
+
+            break ; 
+        }
+        
+
+        }
+
+        if (all_empty(liste_bool_F, nbF)) {
+
+            cout <<  j << "       " << j << "      " << "1 0" << endl ;  
+            break ; 
+        }
+        
+
+    }
+}    
+
+
+
+void affiche_attente_moyenne(int nbF, vector<double> attente_moyenne_NEQLI, vector<double> attente_moyenne_FANEQLI, vector<int> taille_queue) {
 
     cout << "Attente moyenne" << endl ; 
     cout << setprecision(2) << fixed;
@@ -284,7 +407,8 @@ void affiche_attente_moyenne(int nbF, vector<double> attente_moyenne_NEQLI, vect
         
         if (taille_queue[i] != 0) {
 
-            cout << attente_moyenne_NEQLI[i]/taille_queue[i] << endl ;
+            cout << attente_moyenne_NEQLI[i]/taille_queue[i] << "   ";
+            cout << attente_moyenne_FANEQLI[i]/taille_queue[i] << endl ; 
             } else {
 
             cout << endl ; 
@@ -297,26 +421,26 @@ void affiche_attente_moyenne(int nbF, vector<double> attente_moyenne_NEQLI, vect
 
 int main () {
 
-    int nbF = 0  ; 
-    int nbC1 = 0 ; 
-    int nbC2 = 0 ;
-    int d1 = 0 ; 
-    int d2 = 0 ; 
+    int nbF = 0, nbC1 = 0, nbC2 = 0, d1 = 0, d2 = 0  ; 
     string display_type ; 
 
     verification_1(display_type, nbF) ;
 
     vector<queue<int> > liste_queues(nbF);
-    vector<bool> liste_bool(nbF);
+    vector<queue<int> > liste_queues_F(nbF) ; 
+    vector<bool> liste_bool_N(nbF);
+    vector<bool> liste_bool_F(nbF);
+    vector<bool> done(nbF, false) ; 
     vector<double> attente_moyenne_NEQLI ( nbF, 0) ;
+    vector<double> attente_moyenne_FANEQLI ( nbF, 0) ;
     vector<int> taille_queue( nbF, 0) ; 
 
-    verification_2_liste_queues(liste_queues, nbF, taille_queue) ;
-    construction_liste_bool(liste_queues, nbF , liste_bool);
+    verification_2_liste_queues(liste_queues, liste_queues_F, nbF, taille_queue) ;
+    construction_liste_bool(liste_queues, nbF , liste_bool_N, liste_bool_F);
 
     affiche_etat_initial(liste_queues, nbF) ;
 
-    if (!all_empty(liste_bool, nbF)) {
+    if (all_empty(liste_bool_N, nbF)) {
 
         if (display_type == "SHOW_CYCLES"){
 
@@ -325,17 +449,18 @@ int main () {
 
     } else {
         
-        NEQLI(liste_queues, liste_bool, nbC1, nbF, d1, attente_moyenne_NEQLI, display_type) ;
+        NEQLI(liste_queues, liste_bool_N, nbC1, nbF, d1, attente_moyenne_NEQLI, display_type) ;
+        FANEQLI(liste_queues_F, liste_bool_F, done, nbC2, nbF, d2, attente_moyenne_FANEQLI, display_type) ;
 
     }
  
     cout << "Nombre de cycles" << endl ; 
-    cout << nbC1 << endl ; 
+    cout << nbC1 << "      " << nbC2 << endl ; 
 
     cout << "Déplacement total" << endl ;
-    cout << d1 << endl ; 
+    cout << d1 << "       " << d2 << endl ; 
 
-    affiche_attente_moyenne(nbF, attente_moyenne_NEQLI, taille_queue) ; 
+    affiche_attente_moyenne(nbF, attente_moyenne_NEQLI, attente_moyenne_FANEQLI, taille_queue) ; 
 
 }
 
